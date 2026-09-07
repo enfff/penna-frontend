336e66a7c1e5e7878986a983b5f1a93cc53c0d2e
enfff <enforcetitan@gmail.com>
2026-09-07 12:39:29 +0200

    chore: remove stray loop log artifacts from the repository

---
+++ src/config.rs
@@ -0,0 +1,4 @@
+pub static VERSION: &str = "0.1.4";
+pub static GETTEXT_PACKAGE: &str = "penna-frontend";
+pub static LOCALEDIR: &str = "/home/enf/Projects/.gnome-builder/projects/penna-frontend/install/host/share/locale";
+pub static PKGDATADIR: &str = "/home/enf/Projects/.gnome-builder/projects/penna-frontend/install/host/share/penna-frontend";
